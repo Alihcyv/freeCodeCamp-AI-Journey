1de9b8273fa80f0b96389bdfcd3036fe258e1415
Alihcyv <elihcyv@gmail.com>
2026-05-10 23:34:21 +0400

    Add data loading functions for CIFAR10 dataset

---
+++ vit_implementation/data.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+def get_dataloaders(batch_size):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    train_dataset = datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root='data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
